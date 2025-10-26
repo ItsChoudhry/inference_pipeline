f1a813e6618c0ee5fb4caf4f847de6fb42c85d04
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2025-10-26 23:17:18 +0000

    Add creating tasks

--- services/api/main.py
+++ services/api/main.py
@@ -1,6 +1,6 @@
 from contextlib import asynccontextmanager
-from typing import Union
-from .models.task import Task
+from typing import Optional, Union
+from .models.task import CreateTask, Task
 from datetime import datetime
 import uuid
 
@@ -42,8 +42,34 @@ def read_root():
     return {"Hello": "World"}
 
 
+@app.post("/v1/tasks")
+def add_task(task: CreateTask, idempotency_key: Optional[str] = None):
+    print(task)
+    print(idempotency_key)
+    if idempotency_key and idempotency_key in tasks:
+        return tasks[idempotency_key]
+
+    now = datetime.now()
+    generated_id = str(uuid.uuid4())
+    idem_key = idempotency_key if idempotency_key else generated_id
+    new_task = Task(
+        id=generated_id,
+        idempotency_key=idem_key,
+        model=task.model,
+        param=task.param,
+        inputs=task.inputs,
+        status="pending",
+        result_url=None,
+        error=None,
+        callback_url=task.callback_url,
+        api_key_id="",
+        created_at=now,
+        updated_at=now,
+    )
+    tasks[idem_key] = new_task
+    return generated_id
+
+
 @app.get("/v1/tasks/{task_id}")
-def read_item(task_id: int, q: Union[str, None] = None):
-    if task_id == 0:
-        return tasks[str(task_id)]
-    return {"item_id": task_id, "q": q}
+def get_task(task_id: str, q: Union[str, None] = None):
+    return tasks[task_id]
--- services/api/models/task.py
+++ services/api/models/task.py
@@ -1,17 +1,25 @@
+from typing import Optional
 from pydantic import BaseModel
 from datetime import datetime
 
 
 class Task(BaseModel):
     id: str
-    idempotency_key: str
+    idempotency_key: Optional[str] = None
     model: str
     param: dict[str, str]
     inputs: dict[str, str]
     status: str
-    result_url: str
-    error: str
-    callback_url: str
-    api_key_id: str
+    result_url: Optional[str] = None
+    error: Optional[str] = None
+    callback_url: Optional[str] = None
+    api_key_id: Optional[str] = None
     created_at: datetime
     updated_at: datetime
+
+
+class CreateTask(BaseModel):
+    model: str
+    param: dict[str, str]
+    inputs: dict[str, str]
+    callback_url: Optional[str] = None
