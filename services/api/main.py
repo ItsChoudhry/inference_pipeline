ee46c08aa1cb039a4ed7c951523fa3c272c9e856
itsChoudhry <23042676+ItsChoudhry@users.noreply.github.com>
2025-11-06 20:48:22 +0000

    Add more apis doc, healthz, and readyz

--- services/api/main.py
+++ services/api/main.py
@@ -1,10 +1,13 @@
 from contextlib import asynccontextmanager
 from typing import Optional, Union
+
+from starlette.responses import JSONResponse
 from .models.task import CreateTask, Task
 from datetime import datetime
 import uuid
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.openapi.docs import get_swagger_ui_html
 
 tasks: dict[str, Task] = {}
 
@@ -78,3 +81,30 @@ def get_task(task_id: str, q: Union[str, None] = None):
 @app.get("/v1/models")
 def list_models():
     return {"ChatBpd": ["0.1.0"], "Cloudsunut": ["0.2.1"]}
+
+
+@app.get("/healthz")
+def health_check():
+    try:
+        # _perform_health_checks()
+        return JSONResponse(status_code=200, content={"status": "healthy"})
+    except Exception as e:
+        raise HTTPException(status_code=503, detail={"status": "unhealthy", "error": e})
+
+
+@app.get("/readyz")
+def ready_check():
+    # _perform_health_checks()
+    # check db, redis ping, minIO access check
+    return JSONResponse(status_code=200, content={"status": "healthy"})
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    """Custom OpenAPI docs at /docs with Swagger UI."""
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title + " - Swagger UI",
+        swagger_js_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui-bundle.js",
+        swagger_css_url="https://cdn.jsdelivr.net/npm/swagger-ui-dist@5/swagger-ui.css",
+    )
